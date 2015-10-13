4cdb962de2d435ac05ebc9d67051288b12cf889c
pstessel <pstessel@gmail.com>
2015-10-13 00:17:18 -0400

    p.56.

--- text_analysis_05.R
+++ text_analysis_05.R
@@ -39,16 +39,25 @@ whale.l <- lapply(chapter.freqs.l, '[', 'whale')
 whales.m <- do.call(rbind, whale.l)
 ahab.l <- lapply(chapter.freqs.l, '[', 'ahab')
 ahabs.m <- do.call(rbind, ahab.l)
+i.l <- lapply(chapter.freqs.l, '[', 'i')
+i.m <- do.call(rbind, i.l)
+my.l <- lapply(chapter.freqs.l, '[', 'my')
+my.m <- do.call(rbind, my.l)
 whales.v <- as.vector(whales.m[,1])
 ahabs.v <- as.vector(ahabs.m[,1])
-whales.ahabs.m <- cbind(whales.v, ahabs.v)
-colnames(whales.ahabs.m) <- c("whale", "ahab")
+i.v <- as.vector(i.m[,1])
+my.v <- as.vector(my.m[,1])
+whales.ahabs.i.my.m <- cbind(whales.v, ahabs.v, i.v, my.v)
+colnames(whales.ahabs.i.my.m) <- c("whale", "ahab", "i", "my")
 
-whales.ahabs.m[which(is.na(whales.ahabs.m))] <- 0
+whales.ahabs.i.my.m[which(is.na(whales.ahabs.i.my.m))] <- 0
+whales.ahabs.i.my.m
 
 cor(whales.ahabs.m)
 mycor <- cor(whales.ahabs.m[, "whale"], whales.ahabs.m[, "ahab"])
 mycor
+
+cor(whales.ahabs.i.my.m)
 
 ### Discursion on Data Frames
 x <- matrix(1,3,3)
@@ -68,4 +77,39 @@ x.df$V2
 
 # make life easier and convert to a data frame
 cor.data.df <- as.data.frame(whales.ahabs.m)
-s
+
+# randomize one of the columns
+sample(cor.data.df$whale)
+
+# correlate randomized values against the ordered values in the unshuffled "ahab" column.
+cor(sample(cor.data.df$whale), cor.data.df$ahab)
+
+# use a for loop to test the correlation over 10,000 samples
+mycors.v <- NULL
+for(i in 1:10000){
+  mycors.v <- c(mycors.v, cor(sample(cor.data.df$whale), cor.data.df$ahab))
+}
+min(mycors.v)
+max(mycors.v)
+range(mycors.v)
+mean(mycors.v)
+sd(mycors.v)
+
+# plot to show the distribution of all the values in mycors.v
+h <- hist(mycors.v, breaks=100, col="grey",
+          xlab="Correlation Coefficient",
+          main="Histrogram of Random Correlation Coefficients\n
+          with Normal Curv",
+          plot=T)
+xfit <- seq(min(mycors.v), max(mycors.v), length=1000)
+yfit <- dnorm(xfit, mean=mean(mycors.v), sd=sd(mycors.v))
+yfit <- yfit*diff(h$mids[1:2])*length(mycors.v)
+lines(xfit, yfit, col="black", lwd=2)
+
+?cor.test
+
+cor.test(cor.data.df$whale,cor.data.df$ahab, method = "pearson")
+
+## Practice 5.1
+# Adding two more columns to the matrix (as per above) with data for the words
+# "i" and "my" tells me that those two words are highly correlated (0.7739595).
